f017c1134038a4c1bced0cce94227601a9751061
RubyLegend <SHatsanIK01@office365.fiot.kpi.ua>
2022-04-14 15:23:03 +0300

    New car control script

--- Assets/Scripts/CarControl.cs
+++ Assets/Scripts/CarControl.cs
@@ -1,81 +1,85 @@
+using System;
 using UnityEngine;
 using System.Collections;
-public class CarControl : MonoBehaviour {
-    public WheelCollider WheelFL;
-    public WheelCollider WheelFR;
-    public WheelCollider WheelRL;
-    public WheelCollider WheelRR;
-    public Transform WheelFLtrans;
-    public Transform WheelFRtrans;
-    public Transform WheelRLtrans;
-    public Transform WheelRRtrans;
-    public Vector3 eulertest;
-    float maxFwdSpeed = -3000;
-    float maxBwdSpeed = 1000f;
-    float gravity = 9.8f;
-    private bool braked = false;
-    private float maxBrakeTorque = 500;
-    private Rigidbody rb;
-    public Transform centreofmass;
-    private float maxTorque = 1000;
-    void Start () 
+public class CarControl : MonoBehaviour
+{
+
+    private const string horizontal = "Horizontal", vertical = "Vertical";
+
+    [SerializeField] private WheelCollider FLCollider, FRCollider, RLCollider, RRCollider;
+    [SerializeField] private Transform FLWheel, FRWheel, RLWheel, RRWheel;
+
+    [SerializeField] private float motorTorque = 200, brakeTorque = 400, steerAngle = 30;
+    [SerializeField] private float WheelBaseLength = 500, WheelDistance = 200;
+    private float horizontalInput, verticalInput,
+                  currentBreakForce, currentSteerAngle;
+    private bool isBrake;
+
+    private float turnRadius;
+    
+    void FixedUpdate ()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.centerOfMass = centreofmass.transform.localPosition;
+        GetInput();
+        HandleVelocity();
+        HandleSteering();
+        UpdateWheels();
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis(horizontal);
+        verticalInput = Input.GetAxis(vertical);
+        isBrake = Input.GetKey(KeyCode.Space);
+        //Debug.Log(horizontalInput);
+        //Debug.Log(verticalInput);
+        //Debug.Log(isBrake);
+    }
+
+    private void HandleVelocity()
+    {
+        RLCollider.motorTorque = verticalInput * motorTorque;
+        RRCollider.motorTorque = verticalInput * motorTorque;
+        currentBreakForce = isBrake ? brakeTorque : 0;
+        HandleBreak();
+    }
+
+    private void HandleBreak()
+    {
+        FLCollider.brakeTorque = currentBreakForce;
+        FRCollider.brakeTorque = currentBreakForce;
+        RLCollider.brakeTorque = currentBreakForce;
+        RRCollider.brakeTorque = currentBreakForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = steerAngle * horizontalInput;
+        turnRadius = Mathf.Abs(WheelBaseLength * Mathf.Tan(Mathf.Deg2Rad*(90 - Mathf.Abs(currentSteerAngle))));
+        float angle1 = currentSteerAngle > 0 ? Mathf.Rad2Deg * Mathf.Atan(WheelBaseLength / (turnRadius + WheelDistance / 2f))
+                : (-1)*Mathf.Rad2Deg * Mathf.Atan(WheelBaseLength / (turnRadius - WheelDistance / 2f));
+        float angle2 = currentSteerAngle > 0 ? Mathf.Rad2Deg * Mathf.Atan(WheelBaseLength / (turnRadius - WheelDistance / 2f))
+                : (-1)*Mathf.Rad2Deg * Mathf.Atan(WheelBaseLength / (turnRadius + WheelDistance / 2f));
+        Debug.Log(angle1);
+        Debug.Log(angle2);
+        FLCollider.steerAngle = angle1;
+        FRCollider.steerAngle = angle2;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateWheelRotation(FLCollider, FLWheel);
+        UpdateWheelRotation(FRCollider, FRWheel);
+        UpdateWheelRotation(RLCollider, RLWheel);
+        UpdateWheelRotation(RRCollider, RRWheel);
+    }
+    private void UpdateWheelRotation(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 angle;
+        Quaternion rotation;
+        wheelCollider.GetWorldPose(out angle, out rotation);
+        wheelTransform.rotation = rotation;
+        wheelTransform.position = angle;
+        
     }
     
-   void FixedUpdate () {
-     if(!braked){
-            WheelFL.brakeTorque = 0;
-            WheelFR.brakeTorque = 0;
-            WheelRL.brakeTorque = 0;
-            WheelRR.brakeTorque = 0;
-        }
-        //speed of car, Car will move as you will provide the input to it.
-   
-      WheelRR.motorTorque = maxTorque * Input.GetAxis("Vertical");
-        WheelRL.motorTorque = maxTorque * Input.GetAxis("Vertical");
-      
-        //changing car direction
-//Here we are changing the steer angle of the front tyres of the car so that we can change the car direction.
-        WheelFL.steerAngle = 30 * (Input.GetAxis("Horizontal"));
-        WheelFR.steerAngle = 30 * Input.GetAxis("Horizontal");
-    }
-    void Update()
-    {
-        HandBrake();
-        
-        //for tyre rotate
-        WheelFLtrans.Rotate(WheelFL.rpm/60*360*Time.deltaTime ,0,0);
-        WheelFRtrans.Rotate(WheelFR.rpm/60*360*Time.deltaTime ,0,0);
-        WheelRLtrans.Rotate(WheelRL.rpm/60*360*Time.deltaTime ,0,0);
-        WheelRRtrans.Rotate(WheelRL.rpm/60*360*Time.deltaTime ,0,0);
-        //changing tyre direction
-        Vector3 temp = WheelFLtrans.localEulerAngles;
-        Vector3 temp1 = WheelFRtrans.localEulerAngles;
-        temp.y = WheelFL.steerAngle - (WheelFLtrans.localEulerAngles.z);
-        WheelFLtrans.localEulerAngles = temp;
-        temp1.y = WheelFR.steerAngle - WheelFRtrans.localEulerAngles.z;
-        WheelFRtrans.localEulerAngles = temp1;
-        eulertest = WheelFLtrans.localEulerAngles;
-    }
-    void HandBrake()
-    {
-        //Debug.Log("brakes " + braked);
-        if(Input.GetButton("Jump"))
-        {
-            braked = true;
-        }
-        else
-        {
-            braked = false;
-        }
-        if(braked){
-         
-            WheelRL.brakeTorque = maxBrakeTorque * 20;//0000;
-            WheelRR.brakeTorque = maxBrakeTorque * 20;//0000;
-            WheelRL.motorTorque = 0;
-            WheelRR.motorTorque = 0;
-        }
-    }
 }
